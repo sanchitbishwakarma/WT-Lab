011aede911b1916e99b967ee6d910145508e7845
JobLessGod <iamjoblessgod@gmail.com>
2026-01-22 01:04:29 +0545

    feat: add HTML structure and JavaScript functionality for Lab 12 - JS Objects

---
+++ Practicals/LAB_12/README.md
@@ -0,0 +1,8 @@
+## Lab 12 : JS Objects
+
+1.⁠ ⁠Create an object to represent a car.
+2.⁠ ⁠Access any property of an object and display its value in popup window.
+3.⁠ ⁠Modify any one property of an object.
+4.⁠ ⁠add a new property to your previously defined object.
+5.⁠ ⁠delete any one property from your object.
+6.⁠ ⁠check if any one property exists in your defined object.
---
+++ Practicals/LAB_12/script.js
@@ -0,0 +1,72 @@
+const MyCar = {
+    fullName: "Porsche 911 GT3 RS",
+    model: "911",
+    variant: "GT3 RS",
+    manufacturer: "Porsche",
+    year: 2022,
+    color: "Guards Red",
+};
+
+function displayCarInfo(car = MyCar) {
+    let displayBoard = document.getElementById("displayBoard");
+    displayBoard.innerHTML = "";
+
+    for (let info in car) {
+        let list = document.createElement("li");
+        list.appendChild(document.createTextNode(car[info]))
+
+        // edit button
+        let edit = document.createElement("button");
+        edit.textContent = "Edit"
+        edit.className = "btn"
+        edit.addEventListener("click", () => modifyProperty(list.firstChild))
+
+        // delete button
+        let deleteButton = document.createElement("button");
+        deleteButton.textContent = "Delete"
+        deleteButton.className = "btn"
+
+        deleteButton.addEventListener("click", () => deleteElement(info));
+
+        // just adding those button into each line
+        list.appendChild(edit);
+        list.appendChild(deleteButton);
+
+        // just return final thing
+        displayBoard.appendChild(list);
+    }
+
+    // user can also add their new property
+    let addNew = document.createElement("button");
+    addNew.textContent = "Add New Property"
+    displayBoard.appendChild(addNew);
+    addNew.addEventListener("click", () => addNewElement())
+    return;
+}
+
+function modifyProperty(el) {
+    let newData = prompt("Enter new information: ");
+    if (newData == "" || newData == null) return;
+    el.textContent = newData;
+    return
+}
+
+function addNewElement() {
+    let newPropertyName = prompt("Enter new property name: ");
+    if (newPropertyName == "" || newPropertyName == null) return;
+    if (MyCar.hasOwnProperty(newPropertyName)) {
+        alert("Property Name already exists! Try Again")
+        return addNewElement()
+    }
+
+    let newData = prompt("Enter new information: ");
+    if (newData == "" || newData == null) return;
+
+    MyCar[newPropertyName] = newData;
+    displayCarInfo()
+}
+
+function deleteElement(property) {
+    delete MyCar[property]
+    displayCarInfo()
+}
